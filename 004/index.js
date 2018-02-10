a2595fee8c984cb2bb3fdf3f3921980a9efc950b
shigyo daisuke <shigyo@colopl.co.jp>
2018-02-10 15:35:07 +0900

    ファイル004の処理とその実行結果ファイル

--- 004/index.js
+++ 004/index.js
@@ -1,5 +1,29 @@
 let fs   = require('fs');
 let path = require('path');
 
+let d = new Date();
+let f = formatDate(d);
 
-console.log(__dirname);
+//__dirname 実行中のスクリプトの絶対パス
+//出力させたいパスとファイル名を設定
+let p = path.join(__dirname, 'out.txt');
+
+fs.writeFile(p, f, (error) => {
+	if(error != null){
+		throw error;
+	}
+
+	console.log('output success!');
+});
+
+
+
+function formatDate(date, delimiter = '/'){
+    let y = date.getFullYear();  // 四桁の年
+    let m = date.getMonth() + 1; // 月（０始まり）
+    let d = date.getDate();      // 日
+    let h = date.getHours();     // 時
+    let i = date.getMinutes();   // 分
+    let s = date.getSeconds();   // 秒
+    return y + delimiter + m + delimiter + d + ' ' + h + ':' + i + ':' + s;
+}
